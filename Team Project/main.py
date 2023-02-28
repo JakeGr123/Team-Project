b99681c09d65d3d05f0c20618586315999ed937d
Jake Grangier <116171342+JakeGr123@users.noreply.github.com>
2023-02-28 10:18:28 -0500

    Add files via upload

--- Team Project/main.py
+++ Team Project/main.py
@@ -4,23 +4,47 @@
 import pygame
 import os
 import sys
+import random
 from tkinter import *
 import customtkinter
-
+ANGLE = 0
+BLACK = (0, 0, 0)
 SIZE = 40
 BACKGROUND = pygame.image.load('Background.png')
 BACKGROUND = pygame.transform.scale(BACKGROUND, (500, 500))
 def game():
+    class Apple:
+        def __init__(self, parent_screen):
+            self.parent_screen = parent_screen
+            self.image = pygame.image.load("monkey.png").convert()
+            self.image = pygame.transform.scale(self.image, (50, 50 ))
+            self.image.set_colorkey(BLACK)
+            self.x = 120
+            self.y = 120
+
+        def draw(self):
+            self.parent_screen.blit(self.image, (self.x, self.y))
+            pygame.display.flip()
+
+        def move(self):
+            self.x = random.randint(1, 24) * SIZE
+            self.y = random.randint(1, 19) * SIZE
 
     class Snake:
         def __init__(self):
-            self.image = pygame.image.load("snake block.jpg").convert()
-            self.image = pygame.transform.scale(self.image, (50, 50))
+            self.head_image = pygame.image.load('snake head.jpg').convert()
+            self.head_image = pygame.transform.scale(self.head_image, (50, 50))
+            self.head_image.set_colorkey(BLACK)
+            self.body_image = pygame.image.load("snake block.jpg").convert()
+            self.body_image = pygame.transform.scale(self.body_image, (50, 50))
+            self.body_image.set_colorkey(BLACK)
             self.direction = 'down'
             self.length = 1
-            self.x = [40]
-            self.y = [40]
-            
+            self.head_x = 40
+            self.head_y = 40
+            self.body_x = [0 * (self.length - 1)] 
+            self.body_y = [0 * (self.length - 1)] 
+
         def move_left(self):
             self.direction = 'left'
 
@@ -35,73 +59,91 @@ def game():
 
         def walk(self):
             # update body
-            for i in range(self.length-1,0,-1):
-                self.x[i] = self.x[i-1]
-                self.y[i] = self.y[i-1]
-                
-                
-
+            for i in range(self.length - 2, -1, -1):
+                self.body_x[i + 1] = self.body_x[i]
+                self.body_y[i + 1] = self.body_y[i]
+            self.body_x[0] = self.head_x
+            self.body_y[0] = self.head_y
 
             # update head
             if self.direction == 'left':
-                self.x[0] -= SIZE
+                self.head_x -= SIZE
             if self.direction == 'right':
-                self.x[0] += SIZE
+                self.head_x += SIZE
             if self.direction == 'up':
-                self.y[0] -= SIZE
+                self.head_y -= SIZE
             if self.direction == 'down':
-                self.y[0] += SIZE
+                self.head_y += SIZE
 
+            if self.head_x < 0:
+                pygame.quit()
 
-            if self.x[0] < 0 or self.x[0] >= 500 or self.y[0] < 0 or self.y[0] >= 500:
-                print('You Lost Monkey')
+            if self.head_x >= 500:
+                pygame.quit()
+
+            if self.head_y < 0:
+                pygame.quit()
+
+            if self.head_y >= 500:
                 pygame.quit()
 
         def draw(self, surface):
-            for i in range(self.length):
-                surface.blit(self.image, (self.x[i], self.y[i]))
+            surface.blit(self.head_image, (self.head_x, self.head_y))
+            for i in range(self.length - 1):
+                surface.blit(self.body_image, (self.body_x[i], self.body_y[i]))
 
     class Game:
         def __init__(self):
             self.surface = pygame.display.set_mode((500, 500))
             pygame.display.set_caption('Snake Game')
             self.snake = Snake()
+            self.apple = Apple(self.surface)
+            
+        def reset(self):
+            self.snake = Snake()
+            self.apple = Apple(self.surface)
 
-        def run(self):
-            clock = pygame.time.Clock()
-            running = True
-            while running:
-                clock.tick(10)
-        
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        running = False
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            running = False
-                        elif event.key == pygame.K_LEFT:
-                            self.snake.move_left()
-                        elif event.key == pygame.K_RIGHT:
-                            self.snake.move_right()
-                        elif event.key == pygame.K_UP:
-                            self.snake.move_up()
-                        elif event.key == pygame.K_DOWN:
-                            self.snake.move_down()
+        def is_collision(self, x1, y1, x2, y2):
+            if x1 >= x2 and x1 < x2 + SIZE:
+                if y1 >= y2 and y1 < y2 + SIZE:
+                    return True
+            return False
 
-                self.surface.blit(BACKGROUND, (0, 0))
-                self.snake.walk()
-                self.snake.draw(self.surface)
-                pygame.display.update()
-
-            pygame.quit()
-
-    def play_game():
-        pygame.init()
-        game = Game()
-        game.run()
+        def play(self):
+            self.apple.draw()
 
     if __name__ == '__main__':
-        play_game()
+        pygame.init()
+        game1 = Game()
+
+        clock = pygame.time.Clock()
+
+        running = True
+        while running:
+            clock.tick(10)
+           
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                    elif event.key == pygame.K_LEFT:
+                        game1.snake.move_left()
+                    elif event.key == pygame.K_RIGHT:
+                        game1.snake.move_right()
+                    elif event.key == pygame.K_UP:
+                        game1.snake.move_up()
+                    elif event.key == pygame.K_DOWN:
+                        game1.snake.move_down()
+
+            game1.surface.blit(BACKGROUND, (0, 0))
+            game1.play()
+            game1.snake.walk()
+            game1.snake.draw(game1.surface)
+            pygame.display.update()
+
+        pygame.quit()
 
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('blue')
@@ -110,14 +152,9 @@ def Play():
     root.withdraw()
     game()
 root = customtkinter.CTk()
-
-
 root.geometry('400x500')
 
 button = customtkinter.CTkButton(master=root, text='Play', command=Play, hover_color='purple', corner_radius=70, height=50,)
 button.place(relx=0.5, rely=0.5, anchor=CENTER)                                    
 
 root.mainloop()
-        
-
-    
---
+++ Team Project/snake.py
@@ -0,0 +1,221 @@
+import pygame
+import random
+score = 0
+angle = 0
+SIZE = 40
+BACKGROUND = pygame.image.load('Background.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND, (500, 500))
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("monkey.png").convert()
+        self.x = 120
+        self.y = 120
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(0,460)
+        self.y = random.randint(0,460)
+
+class Snake:
+    
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.head_down_image = pygame.image.load('snake head down.png').convert()
+        self.head_down_image = pygame.transform.scale(self.head_down_image, (50, 50))
+        self.head_up_image = pygame.image.load('snake head up.png').convert()
+        self.head_up_image = pygame.transform.scale(self.head_up_image, (50, 50))
+        self.head_left_image = pygame.image.load('snake head left.png').convert()
+        self.head_left_image = pygame.transform.scale(self.head_left_image, (50, 50))
+        self.head_right_image = pygame.image.load('snake head right.png').convert()
+        self.head_right_image = pygame.transform.scale(self.head_right_image, (50, 50))
+        self.body_image = pygame.image.load("snake block.jpg").convert()
+        self.body_image = pygame.transform.scale(self.body_image, (50, 50))
+        self.direction = 'down'
+        self.length = 1
+        self.head_x = 40
+        self.head_y = 40
+        self.body_x = []
+        self.body_y = []
+        self.moveup = False
+        self.movedown = False
+        self.moveleft = False
+        self.moveright = False
+        self.angle = 0
+
+    def move_left(self):
+        if self.moveright == False:
+            self.angle = 270
+            self.moveleft = True
+            self.direction = 'left'
+            self.moveup = False
+            self.movedown = False
+            self.moveright = False
+            
+            print(self.angle)
+            pygame.display.update()
+
+    def move_right(self):
+        if self.moveleft == False:
+            self.angle = 90
+            self.direction = 'right'
+            self.moveright = True
+            self.moveup = False
+            self.moveleft = False
+            self.movedown = False
+            print(self.angle)
+            
+            pygame.display.update()
+
+    def move_up(self):
+        if self.movedown == False:
+            self.angle = 180
+            self.direction = 'up'
+            self.moveup = True
+            self.movedown = False
+            self.moveleft = False
+            self.moveright = False
+            print(self.angle)
+            
+            pygame.display.update()
+
+    def move_down(self):
+        if self.moveup == False:
+            self.angle = 0
+            self.direction = 'down'
+            self.movedown = True
+            self.moveup = False
+            self.moveleft = False
+            self.moveright = False
+            print(self.angle)
+            
+            pygame.display.update()
+
+    def walk(self):
+        # update body
+        if self.length > 1:
+            self.body_x.append(self.head_x)
+            self.body_y.append(self.head_y)
+            for i in range(self.length-2, -1, -1):
+                self.body_x[i+1] = self.body_x[i]
+                self.body_y[i+1] = self.body_y[i]
+            self.body_x[0] = self.head_x
+            self.body_y[0] = self.head_y
+
+        # update head
+        if self.direction == 'left':
+            self.head_x -= SIZE
+            
+        if self.direction == 'right':
+            self.head_x += SIZE
+        if self.direction == 'up':
+            self.head_y -= SIZE
+        if self.direction == 'down':
+            self.head_y += SIZE
+
+        if self.head_x < 0 or self.head_x >= 500 or self.head_y < 0 or self.head_y >= 500:
+            pygame.quit()
+    def check_collision(self):
+            head_rect = pygame.Rect(self.head_x, self.head_y, SIZE, SIZE)
+            for i in range(1, self.length):
+                body_rect = pygame.Rect(self.body_x[i], self.body_y[i], SIZE, SIZE)
+                if head_rect.colliderect(body_rect):
+                    return True
+            return False
+
+    def draw(self):
+        if angle == 0:
+            self.parent_screen.blit(self.head_down_image, (self.head_x, self.head_y))
+        if angle == 90:
+            self.parent_screen.blit(self.head_right_image, (self.head_x, self.head_y))
+        if angle == 270:
+            self.parent_screen.blit(self.head_left_image, (self.head_x, self.head_y))
+        if angle == 180:
+            self.parent_screen.blit(self.head_up_image, (self.head_x, self.head_y))
+        for i in range(self.length - 1):
+            self.parent_screen.blit(self.body_image, (self.body_x[i], self.body_y[i]))
+        pygame.display.update()
+class Game:
+    def __init__(self):
+        self.surface = pygame.display.set_mode((500, 500))
+        pygame.display.set_caption('Snake Game')
+        self.snake = Snake(self.surface)
+        self.apple = Apple(self.surface)
+
+    def reset(self):
+        self.snake = Snake(self.surface)
+        self.apple = Apple(self.surface)
+
+    def play(self, score):
+        snake_rect = pygame.Rect(self.snake.head_x, self.snake.head_y, SIZE, SIZE)
+        apple_rect = pygame.Rect(self.apple.x, self.apple.y, SIZE, SIZE)
+
+        if snake_rect.colliderect(apple_rect):
+            self.snake.length += 1
+            score += 1
+            self.snake.body_x.append(self.snake.head_x)
+            self.snake.body_y.append(self.snake.head_y)
+            self.apple.move()
+
+        for i in range(self.snake.length - 1):
+            body_rect = pygame.Rect(self.snake.body_x[i], self.snake.body_y[i], SIZE, SIZE)
+
+        if self.snake.head_x < 0 or self.snake.head_x >= 500 or self.snake.head_y < 0 or self.snake.head_y >= 500:
+            pygame.quit()
+
+        self.snake.walk()
+        self.surface.blit(BACKGROUND, (0, 0))
+        self.snake.draw()
+        self.apple.draw()
+        pygame.display.flip()
+
+    def run(self):
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            
+            clock.tick(10)       
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                    elif event.key == pygame.K_LEFT:
+                        self.snake.move_left()
+                        
+                    elif event.key == pygame.K_RIGHT:
+                        self.snake.move_right()
+
+                    elif event.key == pygame.K_UP:
+                        self.snake.move_up()
+
+                    elif event.key == pygame.K_DOWN:
+                        self.snake.move_down()
+                        
+            if score == 5:
+                if event.key == pygame.K_ESCAPE:
+                        running = False
+                elif event.key == pygame.K_LEFT:
+                        self.snake.move_right()
+                        self.snake.moveright = False
+                elif event.key == pygame.K_RIGHT:
+                        self.snake.move_left()
+                        self.snake.moveleft = False
+                elif event.key == pygame.K_UP:
+                        self.snake.move_down()
+                        self.snake.movedown = False
+                elif event.key == pygame.K_DOWN:
+                        self.snake.move_up()
+                        self.snake.moveup = False
+            self.play(score)
+        pygame.quit()
+def play_game():
+        pygame.init()
+        game = Game()
+        game.run()
+if __name__ == '__main__':
+        play_game()
